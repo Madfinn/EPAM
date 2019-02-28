d8f7db0d67bcdd2d37894708dc408e9da6e3c679
AnnaGolovenko <blairw73@gmail.com>
2019-03-01 00:36:36 +0200

    add item

--- EPAM project/js/script.js
+++ EPAM project/js/script.js
@@ -3,6 +3,9 @@ const searchGlass = document.getElementById("searchGlass");
 const searchField = document.getElementById("search");
 const navigationMenu = document.getElementById("navigationMenu");
 const hamburger = document.getElementById("hamburger");
+const showMoreBtn = document.getElementById("showMore");
+const catalogBlock = document.querySelectorAll(".container__catalogRow");
+
 searchGlass.addEventListener("click", function (e) {
     e.preventDefault();
     searchField.classList.toggle("is-show");
@@ -11,4 +14,17 @@ searchGlass.addEventListener("click", function (e) {
 hamburger.addEventListener("click", function (e) {
     e.preventDefault();
     navigationMenu.classList.toggle("is-show");
+});
+
+showMoreBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    for (let i = 5; i <= catalogBlock.length; i++) {
+        if (catalogBlock[i].classList.contains("hidden")) {
+            catalogBlock[i].classList.remove("hidden");
+            showMoreBtn.textContent = "Show more";
+        } else {
+            catalogBlock[i].classList.add("hidden");
+            showMoreBtn.textContent = "Show less";
+        }
+    }
 });
